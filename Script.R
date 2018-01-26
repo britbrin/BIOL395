36bcaa971a09a05934c36b77d742675ca754fdcd
Britt <britbrin@dhcp-v880-17a-00124.1Xwireless.unc.edu>
2018-01-26 16:21:57 -0500

    First try

---
+++ Script.R
@@ -0,0 +1,7 @@
+library(readr)
+# download packages 
+
+uniq_ads_CI_grid_ids_tmin <- read_csv("~/Desktop/SeniorSpring/BIOL395/GitHub/Vireo-olivaceus/uniq_ads_CI_grid_ids_tmin.csv")
+View(uniq_ads_CI_grid_ids_tmin)
+# import HurlbertLiang2012 dataset
+
